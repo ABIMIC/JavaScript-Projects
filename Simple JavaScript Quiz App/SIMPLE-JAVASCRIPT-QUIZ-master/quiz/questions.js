2649f7d918e549dbe9637fc0e92e461da0af4b2a
ABIMIC <abikalemichaelraymond@gmail.com>
2019-11-28 22:24:13 -0800

    Simple JavaScript Quiz App

---
+++ Simple JavaScript Quiz App/SIMPLE-JAVASCRIPT-QUIZ-master/quiz/questions.js
@@ -0,0 +1,9 @@
+var questions = [
+  ["JavaScript is a high end, interpreted language?", "True", "False", "Not Sure","None of the Above", "1"],
+  ["JavaScript was formerly known as", "JQuery", "Netbean", "Mocha", "all","3"],
+  ["JavaScript was named by", "Twitter", "Facebook", "Amazon", "None","4"],
+  [" JavaScript variable helps with storing data", "True", " False", "None of the Above", "All of the above","1"],
+  ["JavaScript Arrays are captured in", "Square Brackets", " Curly Braces", "Regular Brackets", "Asterisk","1"],
+  [" JavaScript ....... is a block of code used to perform a particular task", "array", "block", " function ", "None of the above","3"],
+  ["Which of the following is a JavaScript Framework?", " laravel", "  bootstrap", "django", "JQuery","4"],
+  ];
